c4ee4c2a208e59f50136dcdc71f214178879f4e8
Jonnada Sai Teja <146433770+saitejajonnada@users.noreply.github.com>
2025-04-09 15:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function volume_sphere() {
+	 const radius = parseFloat(document.getElementById("radius").value);
+      if (!isNaN(radius)) {
+        const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        document.getElementById("volume").value = volume.toFixed(2);
+      } else {
+        alert("Please enter a valid number for the radius.");
+      }
+	
     //Write your code here
   
 } 
